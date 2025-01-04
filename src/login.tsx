9e2f891cb897d0ad13ac80e9587968cad69498e8
kaijif <thefuka2@gmail.com>
2025-01-04 15:39:02 -0500

    rounded corner

--- src/login.tsx
+++ src/login.tsx
@@ -66,12 +66,12 @@ export default function SignInPage({ username, setUsername, password, setPasswor
                 <form method="post" onSubmit={handleSubmit}>
                     <p>
                         <label>Username</label><br/>
-                        <input type="text" name="username" required />
+                        <input type="text" name="username" class="login-input" required />
                     </p>
                     <p>
                         <label>Password</label>
                         <br/>
-                        <input type="password" name="password" required />
+                        <input type="password" name="password" class="login-input" required />
                     </p>
                     <p>
                         <button id="sub_btn" type="submit" >Login</button>
